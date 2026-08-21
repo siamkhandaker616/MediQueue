73234c2e3b04dbcf0ef2ae1d16bbac00cef2bf19
Siam Khandaker <siamkhandaker616@gmail.com>
2026-08-21 17:55:40 +0600

    icon fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -40,7 +40,7 @@ class AppServiceProvider extends ServiceProvider
         return [
             ['route' => 'admin.dashboard', 'label' => 'Dashboard', 'icon' => 'M4 5a2 2 0 012-2h12a2 2 0 012 2v14a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 5h10M7 13h6m-6 4h4'],
             ['route' => 'admin.reviews', 'label' => 'Reviews', 'icon' => 'M11.48 3.5a.56.56 0 011.04 0l2.12 5.11.55.35 5.52.44c.5.04.7.66.32.99l-4.2 3.6-.18.56 1.28 5.39a.56.56 0 01-.84.6l-4.72-2.88-.49-.3-.5.3-4.72 2.89a.56.56 0 01-.84-.61l1.29-5.39-.18-.56-4.2-3.6a.56.56 0 01.32-.99l5.51-.44.55-.35 2.13-5.11z'],
-            ['route' => 'admin.analytics', 'label' => 'Analytics', 'icon' => 'M4 20V10m5 10V4m5 16v-8m5 8v-4'],
+            ['route' => 'admin.analytics', 'label' => 'Analytics', 'icon' => 'M3 20h4v-8H3zM10 20h4V4h-4zM17 20h4v-6h-4z'],
             ['route' => 'admin.refunds', 'label' => 'Refunds', 'icon' => 'M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z'],
             ['route' => 'admin.departments.index', 'label' => 'Departments', 'icon' => 'M4 6a2 2 0 012-2h12a2 2 0 012 2v12a2 2 0 01-2 2H6a2 2 0 01-2-2V6zm3 3h4m-4 4h4m6-4h1m-1 4h1'],
             ['route' => 'admin.doctors.index', 'label' => 'Doctors', 'icon' => 'M16 7a4 4 0 11-8 0 4 4 0 018 0zM5 21a7 7 0 0114 0'],
